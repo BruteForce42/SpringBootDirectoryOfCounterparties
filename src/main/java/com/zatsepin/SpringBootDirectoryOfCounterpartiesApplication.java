acba8abba51323d8fc0043b8ff2d03994150a0bc
BruteForce42 <zatsepinevgeniy@gmail.com>
2021-05-28 14:53:35 +0600

    Initial commit

---
+++ src/main/java/com/zatsepin/SpringBootDirectoryOfCounterpartiesApplication.java
@@ -0,0 +1,13 @@
+package com.zatsepin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDirectoryOfCounterpartiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDirectoryOfCounterpartiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zatsepin/SpringBootDirectoryOfCounterpartiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zatsepin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDirectoryOfCounterpartiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
